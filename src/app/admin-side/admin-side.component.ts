9d3938e30a153a2ebcf4c0c63948e1d7e6c6d115
DoraymonIT <bendrimou@gmail.com>
2020-10-17 19:16:33 +0100

    add switching icons

--- src/app/admin-side/admin-side.component.ts
+++ src/app/admin-side/admin-side.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from "@angular/core";
-import {Title} from "@angular/platform-browser";
+import { Title } from "@angular/platform-browser";
+
 @Component({
   selector: "app-admin-side",
   templateUrl: "./admin-side.component.html",
   styleUrls: ["./admin-side.component.css"],
 })
 export class AdminSideComponent implements OnInit {
-  constructor(private titleService:Title) {
+  icon ='down';
+  constructor(private titleService: Title) {
     this.hideMatBadge = false;
     this.hideMatBadgeMessage = false;
     this.ex = 3;
     this.exMessage = 5;
     this.aujourdHui = new Date();
     this.titleService.setTitle("Dashoboard");
-  }
 
+  }
+  changeIcon(){
+ if(this.icon == 'down') {
+       this.icon = 'right';
+     } else {
+       this.icon = 'down';
+     }
+  }
   aujourdHui: Date;
 
   opened: boolean;
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,61 +1,62 @@
-import { NgModule, Component } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginSideComponent } from './login-side/login-side.component';
-import { AdminSideComponent } from './admin-side/admin-side.component';
-import { StudentsComponent } from './admin-side/students/students.component';
-import { OverviewComponent } from './admin-side/overview/overview.component';
-import { TeachersComponent } from './admin-side/teachers/teachers.component';
-import { CreateTeacherComponent } from './admin-side/create-teacher/create-teacher.component';
-import { CreateStudentComponent } from './admin-side/create-student/create-student.component';
-
+import { NgModule, Component } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginSideComponent } from "./login-side/login-side.component";
+import { AdminSideComponent } from "./admin-side/admin-side.component";
+import { StudentsComponent } from "./admin-side/students/students.component";
+import { OverviewComponent } from "./admin-side/overview/overview.component";
+import { TeachersComponent } from "./admin-side/teachers/teachers.component";
+import { CreateTeacherComponent } from "./admin-side/create-teacher/create-teacher.component";
+import { CreateStudentComponent } from "./admin-side/create-student/create-student.component";
 
 const routes: Routes = [
   {
-    path: '',
-    component: LoginSideComponent
+    path: "",
+    component: LoginSideComponent,
   },
   {
-    path: 'admin',
+    path: "admin",
     component: AdminSideComponent,
-    children:[
+    children: [
       {
-       path : '',
-       component: OverviewComponent,
-       outlet:'un'
+        path: "",
+        component: OverviewComponent,
+        outlet: "un",
       },
       {
-       path : 'overview',
-       component: OverviewComponent,
-       outlet: 'un'
-      }, {
-        path: 'students-list',
-        component : StudentsComponent,
-        outlet: 'un'
-      }, {
-        path: 'student-create',
-        component : CreateStudentComponent,
-        outlet: 'un'
-      }, {
-        path: 'teachers-list',
-        component : TeachersComponent,
-        outlet: 'un'
-      }, {
-        path: 'teacher-create',
-        component : CreateTeacherComponent,
-        outlet: 'un'
+        path: "overview",
+        component: OverviewComponent,
+        outlet: "un",
       },
-    ]
-  }
-  ,{
-    path: 'logout',
-    component : LoginSideComponent,
+      {
+        path: "students-list",
+        component: StudentsComponent,
+        outlet: "un",
+      },
+      {
+        path: "student-create",
+        component: CreateStudentComponent,
+        outlet: "un",
+      },
+      {
+        path: "teachers-list",
+        component: TeachersComponent,
+        outlet: "un",
+      },
+      {
+        path: "teacher-create",
+        component: CreateTeacherComponent,
+        outlet: "un",
+      },
+    ],
   },
-
-
+  {
+    path: "logout",
+    component: LoginSideComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
